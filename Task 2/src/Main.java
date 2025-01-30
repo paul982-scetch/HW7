526e0c425b0e6cb5a684d0bee29426eb91869036
paulgithub.8 <Ot_moroz_ochek.8@mail.ru>
2025-01-30 13:06:18 +0500

    увольнение айтишников while

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i < 101; i++){
-            if (i % 2 == 0){
-                System.out.println("Уволен айтишник с id " + i );
-            }
+        int i = 1;
+    while (i >= 1 && i < 101){
+        i++;
+        if (i % 2 == 0){
+            System.out.println("Уволен айтишник с id " + i);
         }
+    }
     }
 }//todo Тут увольняем айтишников
