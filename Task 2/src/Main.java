fe9d859db0bd5f15a282b8a22992b4c9692057df
paulgithub.8 <Ot_moroz_ochek.8@mail.ru>
2025-01-30 12:54:01 +0500

    увольнение айтишников

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //todo Тут увольняем айтишников
+        for (int i = 1; i < 101; i++){
+            if (i % 2 == 0){
+                System.out.println("Уволен айтишник с id " + i );
+            }
+        }
     }
-}
+}//todo Тут увольняем айтишников
